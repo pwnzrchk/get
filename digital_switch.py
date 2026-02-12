3e24c65776df4b339a2515895f4b81047fe51b02
pwnzrchk <nzrchk@icloud.com>
2026-02-12 19:45:37 +0300

    Add digital_switch button script

---
+++ digital_switch.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+LIGHT = 23
+
+GPIO.setup(LIGHT, GPIO.OUT)
+
+SWITCH = 13
+GPIO.setup(SWITCH, GPIO.IN)
+
+STATE = 0
+
+while True:
+    if GPIO.input(SWITCH):
+        STATE = not STATE
+        GPIO.output(LIGHT, STATE)
+        time.sleep(0.2)
